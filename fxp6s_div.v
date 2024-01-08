b7b00bfd86de97af105e9330023aea0fc83344b4
Pagol1 <bonny.tffs@gmail.com>
2024-01-08 20:30:33 +0530

    divider support for variable LSB_POW

---
+++ fxp6s_div.v
@@ -0,0 +1,98 @@
+// IO Params
+`define FXP6S_ADDR 5:0
+`define FXP6S_WIDTH 6
+`define FXP6S_SIGN 5
+`define FXP6S_MAG 4:0
+`define FXP6S_LSB_POW -2
+
+module fxp6s_div(
+	input clk,
+	input rstn,
+	input [`FXP6S_ADDR] in_n,		// Sign-Mag
+	input [`FXP6S_ADDR] in_d,		// Sign-Mag
+	output [`FXP6S_ADDR] out_q,
+	output div_by_zero
+);
+	
+	genvar i;
+	wire [1:0] div_q_mask;
+	assign div_q_mask[0] = in_d[4];
+	generate
+		for (i=1; i<2; i=i+1) begin : DIV_MASK
+			assign div_q_mask[i] = div_q_mask[i] | in_d[4-i];
+		end
+	endgenerate
+	// Stage 0 : 2^2
+	wire [2:0] div_st0_s;
+	wire [2:0] div_st0_b;
+	wire [2:0] div_st0_q;
+	restoring_subtractor RS_lEnd_0(in_n[4], 1'b0, div_st0_b[1], out_q[4], div_st0_s[2], div_st0_b[2], div_st0_q[2]);
+	generate
+		for (i=1; i>=1; i=i-1) begin : RS_ROW_N_NEW
+			restoring_subtractor RS_0(in_n[i+2], in_d[i], div_st0_b[i-1], div_st0_q[i+1], div_st0_s[i], div_st0_b[i], div_st0_q[i]);
+		end
+	endgenerate
+	restoring_subtractor RS_rEnd_0(in_n[2], in_d[0], 1'b0, div_st0_q[1], div_st0_s[0], div_st0_b[0], div_st0_q[0]);
+	assign out_q[4] = ~(div_q_mask[1] | div_st0_b[2]);
+	// Stage 1 : 2^1
+	wire [3:0] div_st1_s;
+	wire [3:0] div_st1_b;
+	wire [3:0] div_st1_q;
+	restoring_subtractor RS_lEnd_1(div_st0_s[2], 1'b0, div_st1_b[2], out_q[3], div_st1_s[3], div_st1_b[3], div_st1_q[3]);
+	generate
+		for (i=2; i>1; i=i-1) begin : RS_ROW_N_USED
+			restoring_subtractor RS_1(div_st0_s[i-1], in_d[i], div_st1_b[i-1], div_st1_q[i+1], div_st1_s[i], div_st1_b[i], div_st1_q[i]);
+		end
+	endgenerate
+	generate
+		for (i=0; i>=1; i=i-1) begin : RS_ROW_N_NEW
+			restoring_subtractor RS_1(in_n[i+1], in_d[i], div_st1_b[i-1], div_st1_q[i+1], div_st1_s[i], div_st1_b[i], div_st1_q[i]);
+		end
+	endgenerate
+	restoring_subtractor RS_rEnd_1(in_n[1], in_d[0], 1'b0, div_st1_q[1], div_st1_s[0], div_st1_b[0], div_st1_q[0]);
+	assign out_q[3] = ~(div_q_mask[0] | div_st1_b[3]);
+	// Stage 2 : 2^0
+	wire [4:0] div_st2_s;
+	wire [4:0] div_st2_b;
+	wire [4:0] div_st2_q;
+	restoring_subtractor RS_lEnd_2(div_st1_s[3], 1'b0, div_st2_b[3], out_q[2], div_st2_s[4], div_st2_b[4], div_st2_q[4]);
+	generate
+		for (i=3; i>1; i=i-1) begin : RS_ROW_N_USED
+			restoring_subtractor RS_2(div_st1_s[i-1], in_d[i], div_st2_b[i-1], div_st2_q[i+1], div_st2_s[i], div_st2_b[i], div_st2_q[i]);
+		end
+	endgenerate
+	generate
+		for (i=0; i>=1; i=i-1) begin : RS_ROW_N_NEW
+			restoring_subtractor RS_2(in_n[i+0], in_d[i], div_st2_b[i-1], div_st2_q[i+1], div_st2_s[i], div_st2_b[i], div_st2_q[i]);
+		end
+	endgenerate
+	restoring_subtractor RS_rEnd_2(in_n[0], in_d[0], 1'b0, div_st2_q[1], div_st2_s[0], div_st2_b[0], div_st2_q[0]);
+	assign out_q[2] = ~div_st2_b[4];
+	// Stage 3 : 2^-1
+	wire [5:0] div_st3_s;
+	wire [5:0] div_st3_b;
+	wire [5:0] div_st3_q;
+	restoring_subtractor RS_lEnd_3(div_st2_s[4], 1'b0, div_st3_b[4], out_q[1], div_st3_s[5], div_st3_b[5], div_st3_q[5]);
+	generate
+		for (i=4; i>1; i=i-1) begin : RS_ROW_N_USED
+			restoring_subtractor RS_3(div_st2_s[i-1], in_d[i], div_st3_b[i-1], div_st3_q[i+1], div_st3_s[i], div_st3_b[i], div_st3_q[i]);
+		end
+	endgenerate
+	restoring_subtractor RS_rEnd_3(1'b0, in_d[0], 1'b0, div_st3_q[1], div_st3_s[0], div_st3_b[0], div_st3_q[0]);
+	assign out_q[1] = ~div_st3_b[5];
+	// Stage 4 : 2^-2
+	wire [5:0] div_st4_s;
+	wire [5:0] div_st4_b;
+	wire [5:0] div_st4_q;
+	restoring_subtractor RS_lEnd_4(div_st3_s[4], 1'b0, div_st4_b[4], out_q[0], div_st4_s[5], div_st4_b[5], div_st4_q[5]);
+	generate
+		for (i=4; i>1; i=i-1) begin : RS_ROW_N_USED
+			restoring_subtractor RS_4(div_st3_s[i-1], in_d[i], div_st4_b[i-1], div_st4_q[i+1], div_st4_s[i], div_st4_b[i], div_st4_q[i]);
+		end
+	endgenerate
+	restoring_subtractor RS_rEnd_4(1'b0, in_d[0], 1'b0, div_st4_q[1], div_st4_s[0], div_st4_b[0], div_st4_q[0]);
+	assign out_q[0] = ~div_st4_b[5];
+	assign div_by_zero = ~|in_d;
+	assign out_q[5] = in_n[5] ^ in_d[5];
+
+endmodule
--- gen_divider.py
+++ gen_divider.py
@@ -73,6 +73,49 @@ def rs_args(st_n, idx):
     ret += ")"
     return ret
 
+## n_i | d_i | b_i | q_i | s_o | b_o | q_o
+def get_rs_args(st_n, idx, use_n, n_id, l_end, r_end):
+    global BIT_LEN
+    ret = "("
+    # n_i
+    if (use_n):
+        ret += "in_n[{}], ".format(n_id)
+    elif not r_end:
+        if isinstance(idx, str):
+            ret += wire_st(st_n-1) + "_s[{}], ".format(idx + "-1")
+        else:
+            ret += wire_st(st_n-1) + "_s[{}], ".format(idx-1)
+    else:
+        ret += "1'b0, "
+    # d_i
+    if not l_end:
+        ret += "in_d[{}], ".format(idx)
+    else:
+        ret += "1'b0, "
+    # b_i
+    if not r_end:
+        if isinstance(idx, str):
+            ret += wire_st(st_n) + "_b[{}], ".format(idx + "-1")
+        else:
+            ret += wire_st(st_n) + "_b[{}], ".format(idx-1)
+    else:
+        ret += "1'b0, "
+    # q_i
+    if l_end:
+        ret += "out_q[{}], ".format(BIT_LEN-2-st_n)
+    else:
+        if isinstance(idx, str):
+            ret += wire_st(st_n) + "_q[{}], ".format(idx+"+1")
+        else:
+            ret += wire_st(st_n) + "_q[{}], ".format(idx+1)
+    # s_o
+    ret += wire_st(st_n) + "_s[{}], ".format(idx)
+    # b_o
+    ret += wire_st(st_n) + "_b[{}], ".format(idx)
+    # q_o
+    ret += wire_st(st_n) + "_q[{}]".format(idx)
+    ret += ");"
+    return ret
 
 def gen_divider():
     global BIT_LEN
@@ -83,8 +126,8 @@ def gen_divider():
     global OUT_PRE
     global LSB_POW
     global MSB_POW
-    assert(BIT_LEN == MSB_POW-LSB_POW+1)
-
+    assert(BIT_LEN-1 == MSB_POW-LSB_POW+1)
+    
     op = ""
     op += pp("// IO Params")
     ADDR = IO_PRE + "ADDR"
@@ -114,125 +157,122 @@ def gen_divider():
     op += pp (");"); INDENT += 1
     op += pp("")
 
-    # Plans:
+    ## MSB_POW == Max +ve power of 2 in expr
     op += pp("genvar i;")
-    for it in range(BIT_LEN-1):
-        op += pp("// Stage {}".format(it))
-        op += pp("wire [{}:0] {}_s;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
-        op += pp("wire [{}:0] {}_b;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
-        op += pp("wire [{}:0] {}_q;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
-        # First and last RS have different IO
-        op += pp( "restoring_subtractor RS_st_{}".format(it) + rs_args(it, "0") + ";" )
+    used_bits = 0
+    used_n = 0
+    mask_bits = 0
+    if (MSB_POW > 1):
+        op += pp("wire [{}:0] div_q_mask;".format(MSB_POW-1))
+        op += pp("assign div_q_mask[0] = in_d[{}];".format(BIT_LEN-2))
         op += pp("generate")
-        op += pp( "for (i=1; i<{}; i=i+1) begin : DIV_STAGE_{}".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, it) )
-        op += pp( "restoring_subtractor RS" + rs_args(it, 'i') + ";" )
+        op += pp("for (i=1; i<{}; i=i+1) begin : DIV_MASK".format(
+                min(MSB_POW, BIT_LEN-2)
+            ))
+        mask_bits = min(MSB_POW, BIT_LEN-2)
+        op += pp("assign div_q_mask[i] = div_q_mask[i] | in_d[{}-i];".format(BIT_LEN-2))
         op += pp("end")
         op += pp("endgenerate")
-        op += pp( "restoring_subtractor RS_end_{}".format(it) + rs_args( it, str(BIT_LEN-1-(it==0)) ) + ";" )
-        op += pp( "assign out_q[{}] = ~{}_b[{}];".format(BIT_LEN-2-it, wire_st(it), BIT_LEN-2 if (it == 0) else BIT_LEN-1) )
-    ## Zero division warning
-    op += pp("assign div_by_zero = ~|in_d;")
-    op += pp("assign out_q[{}] = in_n[{}] ^ in_d[{}];".format(BIT_LEN-1, BIT_LEN-1, BIT_LEN-1))
-    op += "\n"
-    INDENT -= 1
-    op += pp("endmodule")
-    return op
 
-## n_i | d_i | b_i | q_i | s_o | b_o | q_o
-def rs_args_i(st_n, idx):
-    global BIT_LEN
-    ret = "("
-    # n_i
-    if (st_n == 0):
-        ret += "in_n[{}], ".format(idx)
-    elif (idx == 0):
-        ret += "1'b0, "
-    else:
-        ret += wire_st(st_n-1)+"_s[{}], ".format(idx-1)
-    # d_i
-    if (idx == BIT_LEN-1):
-        ret += "1'b0, "
-    else:
-        ret += "in_d[{}], ".format(idx)
-    # b_i
-    if (idx == 0):
-        ret += "1'b0, "
-    else:
-        ret += wire_st(st_n) + "_b[{}], ".format(idx-1)
-    # q_i
-    if (idx == BIT_LEN-1-(st_n==0)):
-        ret += "out_q[{}], ".format(BIT_LEN-2-st_n)
-    elif (idx == 0):
-        ret += wire_st(st_n) + "_q[{}], ".format(1)
-    else:
-        ret += wire_st(st_n) + "_q[{}], ".format(idx+1)
-    # s_o
-    ret += wire_st(st_n) + "_s[{}], ".format(idx)
-    # b_o
-    ret += wire_st(st_n) + "_b[{}], ".format(idx)
-    # q_o
-    ret += wire_st(st_n) + "_q[{}]".format(idx)
-    ret += ")"
-    return ret
+    ## Useless, not gonna be a use-case tbh
+    for idx in range(MSB_POW, MSB_POW-LSB_POW, -1):
+        op += pp("// Stage {} : 2^{}".format(used_bits, idx))
+        op += pp("assign out_q[{}] = 1'b0;".format(BIT_LEN-2-used_bits))
+        used_bits += 1
+        if (used_bits == BIT_LEN-1):
+            op += "\n"; INDENT -= 1
+            op += pp("endmodule")
+            return op
+    # Start from MAX_POW or MAX_POW-LOW_POW if LOW_POW > 0
+    for power in range(MSB_POW-used_bits, 0, -1):
+        n_tgt = MSB_POW-LSB_POW+1 - power # used_n = n_tgt
+        op += pp("// Stage {} : 2^{}".format(used_bits, power))
+        op += pp("wire [{}:0] {}_s;".format(n_tgt-1, wire_st(used_bits)))
+        op += pp("wire [{}:0] {}_b;".format(n_tgt-1, wire_st(used_bits)))
+        op += pp("wire [{}:0] {}_q;".format(n_tgt-1, wire_st(used_bits)))
+        ## Need to take logic one step behind now
+        ## rs_args -> 
+            ### use_n, d_val = idx, b_in = {0, idx-1}
+            ### q_in = {Q[BL-2-used_bits], idx+1}, rest = idx
+        ## Count number of blocks in "group" ==> Add generate
+        if (used_n >= 1):
+            op += pp("restoring_subtractor RS_lEnd_{}".format(used_bits) + get_rs_args(used_bits, n_tgt-1, False, "", True, False))
+        else:
+            op += pp("restoring_subtractor RS_lEnd_{}".format(used_bits) + get_rs_args(used_bits, n_tgt-1, True, f"{MSB_POW-LSB_POW}", True, False))
+        ## Chain Used
+        if (used_n >= 2):
+            op += pp("generate")
+            op += pp( "for (i={}; i>{}; i=i-1) begin : RS_ROW_N_USED".format(n_tgt-2, max(n_tgt-used_n, 1)) )
+            op += pp( "restoring_subtractor RS_{}".format(used_bits) + get_rs_args(used_bits, "i", False, "", False, False) )
+            op += pp( "end" )
+            op += pp("endgenerate")
+        ## Chain Free
+        if (n_tgt > used_n):
+            op += pp("generate")
+            op += pp( "for (i={}; i>={}; i=i-1) begin : RS_ROW_N_NEW".format(n_tgt-used_n-1-(used_n==0), 1) )
+            op += pp( "restoring_subtractor RS_{}".format(used_bits) + get_rs_args(used_bits, "i", True, "i+{}".format(power), False, False) )
+            op += pp( "end" )
+            op += pp("endgenerate")
+            op += pp("restoring_subtractor RS_rEnd_{}".format(used_bits) + get_rs_args(used_bits, 0, True, power, False, True))
+        op += pp("assign out_q[{}] = ~(div_q_mask[{}] | {}_b[{}]);".format(BIT_LEN-2-used_bits, power-1, wire_st(used_bits), n_tgt-1))
+        used_n = n_tgt
+        used_bits += 1
 
-def gen_div():
-    global BIT_LEN
-    global INDENT
-    global MODULE_FILE_NAME
-    global IO_PRE
-    global INT_PRE
-    global OUT_PRE
-    global LSB_POW
-    global MSB_POW
-    assert(BIT_LEN == MSB_POW-LSB_POW+1)
+    for power in range(0, LSB_POW-1, -1):
+        n_tgt = MSB_POW-LSB_POW+1 + (power < 0)
+        op += pp("// Stage {} : 2^{}".format(used_bits, power))
+        op += pp("wire [{}:0] {}_s;".format(n_tgt-1, wire_st(used_bits)))
+        op += pp("wire [{}:0] {}_b;".format(n_tgt-1, wire_st(used_bits)))
+        op += pp("wire [{}:0] {}_q;".format(n_tgt-1, wire_st(used_bits)))
+        if (power == 0):
+            if (used_n >= 1):
+                op += pp("restoring_subtractor RS_lEnd_{}".format(used_bits) + get_rs_args(used_bits, n_tgt-1, False, "", True, False))
+            else:
+                op += pp("restoring_subtractor RS_lEnd_{}".format(used_bits) + get_rs_args(used_bits, n_tgt-1, True, f"{MSB_POW-LSB_POW}", True, False))
+            if (used_n >= 2):
+                op += pp("generate")
+                op += pp( "for (i={}; i>{}; i=i-1) begin : RS_ROW_N_USED".format(n_tgt-2, max(n_tgt-used_n, 1)) )
+                op += pp( "restoring_subtractor RS_{}".format(used_bits) + get_rs_args(used_bits, "i", False, "", False, False) )
+                op += pp( "end" )
+                op += pp("endgenerate")
+            if (n_tgt > used_n):
+                op += pp("generate")
+                op += pp( "for (i={}; i>={}; i=i-1) begin : RS_ROW_N_NEW".format(n_tgt-used_n-1-(used_n==0), 1) )
+                op += pp( "restoring_subtractor RS_{}".format(used_bits) + get_rs_args(used_bits, "i", True, "i+{}".format(power), False, False) )
+                op += pp( "end" )
+                op += pp("endgenerate")
+                op += pp("restoring_subtractor RS_rEnd_{}".format(used_bits) + get_rs_args(used_bits, 0, True, power, False, True))
+            else:
+                op += pp("restoring_subtractor RS_rEnd_{}".format(used_bits) + get_rs_args(used_bits, 0, False, "", False, True))
+            used_n = n_tgt
+        else:
+            op += pp("restoring_subtractor RS_lEnd_{}".format(used_bits) + get_rs_args(used_bits, n_tgt-1, False, "", True, False))
+            op += pp("generate")
+            op += pp( "for (i={}; i>{}; i=i-1) begin : RS_ROW_N_USED".format(n_tgt-2, 1) )
+            op += pp( "restoring_subtractor RS_{}".format(used_bits) + get_rs_args(used_bits, "i", False, "", False, False) )
+            op += pp( "end" )
+            op += pp("endgenerate")
+            op += pp("restoring_subtractor RS_rEnd_{}".format(used_bits) + get_rs_args(used_bits, 0, False, "", False, True))
+        op += pp("assign out_q[{}] = ~{}_b[{}];".format(BIT_LEN-2-used_bits, wire_st(used_bits), n_tgt-1))
+        used_bits += 1
 
-    op = ""
-    op += pp("// IO Params")
-    ADDR = IO_PRE + "ADDR"
-    WIDTH = IO_PRE + "WIDTH"
-    SIGN = IO_PRE + "SIGN"
-    MAG = IO_PRE + "MAG"
-    POW_DECL = IO_PRE + "LSB_POW"
-    op += pp("`define " + ADDR[1:] + " {}:0".format(BIT_LEN-1))
-    op += pp("`define " + WIDTH[1:] + " {}".format(BIT_LEN))
-    op += pp("`define " + SIGN[1:] + " {}".format(BIT_LEN-1))
-    op += pp("`define " + MAG[1:] + " {}:0".format(BIT_LEN-2))
-    op += pp("`define " + POW_DECL[1:] + " {}".format(LSB_POW))
+    ## After one's place
+    #op += pp("genvar i;")
+    #for it in range(BIT_LEN-1):
+    #    op += pp("// Stage {}".format(it))
+    #    op += pp("wire [{}:0] {}_s;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
+    #    op += pp("wire [{}:0] {}_b;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
+    #    op += pp("wire [{}:0] {}_q;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
+    #    # First and last RS have different IO
+    #    op += pp( "restoring_subtractor RS_st_{}".format(it) + rs_args(it, "0") + ";" )
+    #    op += pp("generate")
+    #    op += pp( "for (i=1; i<{}; i=i+1) begin : DIV_STAGE_{}".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, it) )
+    #    op += pp( "restoring_subtractor RS" + rs_args(it, 'i') + ";" )
+    #    op += pp("end")
+    #    op += pp("endgenerate")
+    #    op += pp( "restoring_subtractor RS_end_{}".format(it) + rs_args( it, str(BIT_LEN-1-(it==0)) ) + ";" )
+    #    op += pp( "assign out_q[{}] = ~{}_b[{}];".format(BIT_LEN-2-it, wire_st(it), BIT_LEN-2 if (it == 0) else BIT_LEN-1) )
 
-    # No Params for last stage
-    op += "\n"
-    op += pp("module " + MODULE_FILE_NAME.split(".")[0] + "(")
-    INDENT += 1
-    op += pp("input clk,")
-    op += pp("input rstn,")
-    op += pp("input [" + ADDR + "] in_n,\t\t// Sign-Mag")
-    op += pp("input [" + ADDR + "] in_d,\t\t// Sign-Mag")
-    op += pp("output [" + ADDR + "] out_q,")
-    op += pp("output div_by_zero")
-    # op += pp("output out_overflow")
-    # Local Params
-    INDENT -= 1
-    op += pp (");"); INDENT += 1
-    op += pp("")
-
-    # Plans:
-    op += pp("genvar i;")
-    for it in range(BIT_LEN-1):
-        op += pp("// Stage {}".format(it))
-        op += pp("wire [{}:0] {}_s;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
-        op += pp("wire [{}:0] {}_b;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
-        op += pp("wire [{}:0] {}_q;".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, wire_st(it)))
-        # First and last RS have different IO
-        op += pp( "restoring_subtractor RS_st{}_{}".format(it, 0) + rs_args_i(it, 0) + ";" )
-        for i in range(1, BIT_LEN-1 - (it == 0)):
-            op += pp( "restoring_subtractor RS_st{}_{}".format(it, i) + rs_args_i(it, i) + ";" )
-        # op += pp("generate")
-        # op += pp( "for (i=1; i<{}; i=i+1) begin : DIV_STAGE_{}".format(BIT_LEN-2 if (it == 0) else BIT_LEN-1, it) )
-        # op += pp( "restoring_subtractor RS" + rs_args(it, 'i') + ";" )
-        # op += pp("end")
-        # op += pp("endgenerate")
-        op += pp( "restoring_subtractor RS_st{}_{}".format(it, (BIT_LEN-1-(it==0))) + rs_args_i( it, (BIT_LEN-1-(it==0)) ) + ";" )
-        op += pp( "assign out_q[{}] = ~{}_b[{}];".format(BIT_LEN-2-it, wire_st(it), BIT_LEN-2 if (it == 0) else BIT_LEN-1) )
     ## Zero division warning
     op += pp("assign div_by_zero = ~|in_d;")
     op += pp("assign out_q[{}] = in_n[{}] ^ in_d[{}];".format(BIT_LEN-1, BIT_LEN-1, BIT_LEN-1))
@@ -244,25 +284,12 @@ def gen_div():
 if __name__ == "__main__":
     INDENT = 0
     ###
-    BIT_LEN = 4
+    BIT_LEN = 6
     MODULE_FILE_NAME = "fxp{}s_div.v".format(BIT_LEN)
     IO_PRE = "`FXP{}S_".format(BIT_LEN)
     INT_PRE = "`FXP{}S_DIV_".format(BIT_LEN)
-    LSB_POW = -30
-    ###
-    # BIT_LEN = 4
-    # MODULE_FILE_NAME = "bit4_dadda.v"
-    # IO_PRE = "`BIT4_"
-    # INT_PRE = "`BIT4_DM_"
-    # LSB_POW = -2
-    ###
-    # BIT_LEN = 8
-    # MODULE_FILE_NAME = "bit8_dadda.v"
-    # IO_PRE = "`BIT8_"
-    # INT_PRE = "`BIT8_DM_"
-    # LSB_POW = -7
-    ###
-    MSB_POW = LSB_POW + (BIT_LEN-1)
+    LSB_POW = -2
+    MSB_POW = LSB_POW + (BIT_LEN-1) - 1
 
     # op = gen_divider()
     op = gen_divider()
